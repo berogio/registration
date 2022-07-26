f3d86dd17448678b5165da162dbcbb7d11ef7a80
berogio <giorgi.beridze01e@geolab.edu.ge>
2022-07-26 14:45:18 +0200

    update

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const input = document.querySelector(`.phone`);
+const input = document.querySelector(`.phone-nummber`);
+const keys = document.querySelector(`.keys`)
+const one = document.querySelector(`.key1`)
+const two = document.querySelector(`.key2`)
+const three = document.querySelector(`.key3`)
+
+one.onclick = function write() {
+
+    input.placeholder = `1`;
+}
+
+two.onclick = function write() {
+    input.placeholder = `2`;
+}
+
+three.onclick = function write() {
+    input.placeholder = `3`;
+}

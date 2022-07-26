14f55e71f3b85bab3332ce69ea503aba9b47837f
berogio <giorgi.beridze01e@geolab.edu.ge>
2022-07-26 23:29:14 +0200

    ggg

--- app.js
+++ app.js
@@ -3,16 +3,57 @@ const keys = document.querySelector(`.keys`)
 const one = document.querySelector(`.key1`)
 const two = document.querySelector(`.key2`)
 const three = document.querySelector(`.key3`)
+const four = document.querySelector(`.key4`)
+const five = document.querySelector(`.key5`)
+const six = document.querySelector(`.key6`)
+const seven = document.querySelector(`.key7`)
+const eight = document.querySelector(`.key8`)
+const nine = document.querySelector(`.key9`)
 
 one.onclick = function write() {
-
-    input.placeholder = `1`;
+    const h2 = document.querySelector(".nummber-input");
+    let number = `1`;
+    h2.insertAdjacentHTML(`beforeend`, number);
 }
 
 two.onclick = function write() {
-    input.placeholder = `2`;
+    const h2 = document.querySelector(".nummber-input");
+    let number = "2";
+    h2.insertAdjacentHTML(`beforeend`, number);
 }
 
 three.onclick = function write() {
-    input.placeholder = `3`;
+    const h2 = document.querySelector(".nummber-input");
+    let number = "3";
+    h2.insertAdjacentHTML(`beforeend`, number);
+}
+four.onclick = function write() {
+    const h2 = document.querySelector(".nummber-input");
+    let number = "4";
+    h2.insertAdjacentHTML(`beforeend`, number);
+}
+five.onclick = function write() {
+    const h2 = document.querySelector(".nummber-input");
+    let number = "5";
+    h2.insertAdjacentHTML(`beforeend`, number);
+}
+six.onclick = function write() {
+    const h2 = document.querySelector(".nummber-input");
+    let number = "6";
+    h2.insertAdjacentHTML(`beforeend`, number);
+}
+seven.onclick = function write() {
+    const h2 = document.querySelector(".nummber-input");
+    let number = "7";
+    h2.insertAdjacentHTML(`beforeend`, number);
+}
+eight.onclick = function write() {
+    const h2 = document.querySelector(".nummber-input");
+    let number = "8";
+    h2.insertAdjacentHTML(`beforeend`, number);
+}
+nine.onclick = function write() {
+    const h2 = document.querySelector(".nummber-input");
+    let number = "9";
+    h2.insertAdjacentHTML(`beforeend`, number);
 }
